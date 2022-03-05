055de21062218212ca6571f8b9513429a21a91c8
Mootassam <mootassame@gmail.com>
2022-03-05 01:37:02 +0100

    signup page

--- frontend/src/view/auth/SignupPage.tsx
+++ frontend/src/view/auth/SignupPage.tsx
@@ -76,55 +76,196 @@ function SignupPage() {
         })`,
       }}
     >
-      <Content>
-        <Logo>
-          {logoUrl ? (
-            <img
-              src={logoUrl}
-              width="240px"
-              alt={i18n('app.title')}
-            />
-          ) : (
-            <h1>{i18n('app.title')}</h1>
-          )}
-        </Logo>
-
-        <FormProvider {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)}>
-            <InputFormItem
-              name="email"
-              placeholder={i18n('user.fields.email')}
-              autoComplete="email"
-              autoFocus
-              externalErrorMessage={externalErrorMessage}
-            />
-
-            <InputFormItem
-              name="password"
-              placeholder={i18n('user.fields.password')}
-              autoComplete="password"
-              type="password"
-            />
-            <button
-              className="btn btn-block btn-primary"
-              type="submit"
-              disabled={loading}
-            >
-              <ButtonIcon loading={loading} />{' '}
-              {i18n('auth.signup')}
-            </button>
-
-            <OtherActions>
-              <Link
-                className="btn btn-sm btn-link"
-                to="/auth/signin"
+      <div className="login-root">
+        <div
+          className="box-root flex-flex flex-direction--column"
+          style={{ minHeight: '100vh', flexGrow: 1 }}
+        >
+          <div className="loginbackground box-background--white padding-top--64">
+            <div className="loginbackground-gridContainer">
+              <div
+                className="box-root flex-flex"
+                style={{
+                  gridArea: 'top / start / 8 / end',
+                }}
               >
-                {i18n('auth.alreadyHaveAnAccount')}
-              </Link>
-            </OtherActions>
-          </form>
-        </FormProvider>
-      </Content>
+                <div
+                  className="box-root"
+                  style={{
+                    backgroundImage:
+                      'linear-gradient(white 0%, rgb(247, 250, 252) 33%)',
+                    flexGrow: 1,
+                  }}
+                ></div>
+              </div>
+              <div
+                className="box-root flex-flex"
+                style={{ gridArea: '4 / 2 / auto / 5' }}
+              >
+                <div
+                  className="box-root box-divider--light-all-2 animationLeftRight tans3s"
+                  style={{ flexGrow: 1 }}
+                />
+              </div>
+              <div
+                className="box-root flex-flex"
+                style={{ gridArea: '6 / start / auto / 2' }}
+              >
+                <div
+                  className="box-root box-background--blue800"
+                  style={{ flexGrow: 1 }}
+                />
+              </div>
+              <div
+                className="box-root flex-flex"
+                style={{ gridArea: '7 / start / auto / 4' }}
+              >
+                <div
+                  className="box-root box-background--blue animationLeftRight"
+                  style={{ flexGrow: 1 }}
+                />
+              </div>
+              <div
+                className="box-root flex-flex"
+                style={{ gridArea: '8 / 4 / auto / 6' }}
+              >
+                <div
+                  className="box-root box-background--gray100 animationLeftRight tans3s"
+                  style={{ flexGrow: 1 }}
+                />
+              </div>
+              <div
+                className="box-root flex-flex"
+                style={{ gridArea: '2 / 15 / auto / end' }}
+              >
+                <div
+                  className="box-root box-background--cyan200 animationRightLeft tans4s"
+                  style={{ flexGrow: 1 }}
+                />
+              </div>
+              <div
+                className="box-root flex-flex"
+                style={{ gridArea: '3 / 14 / auto / end' }}
+              >
+                <div
+                  className="box-root box-background--blue animationRightLeft"
+                  style={{ flexGrow: 1 }}
+                />
+              </div>
+              <div
+                className="box-root flex-flex"
+                style={{ gridArea: '4 / 17 / auto / 20' }}
+              >
+                <div
+                  className="box-root box-background--gray100 animationRightLeft tans4s"
+                  style={{ flexGrow: 1 }}
+                />
+              </div>
+              <div
+                className="box-root flex-flex"
+                style={{ gridArea: '5 / 14 / auto / 17' }}
+              >
+                <div
+                  className="box-root box-divider--light-all-2 animationRightLeft tans3s"
+                  style={{ flexGrow: 1 }}
+                />
+              </div>
+            </div>
+          </div>
+          <div
+            className="box-root padding-top--24 flex-flex flex-direction--column"
+            style={{ flexGrow: 1, zIndex: 9 }}
+          >
+            <div className="box-root padding-top--48 padding-bottom--24 flex-flex flex-justifyContent--center">
+              <h1>
+                <a
+                  href="http://blog.stackfindover.com/"
+                  rel="dofollow"
+                >
+                  Stackfindover
+                </a>
+              </h1>
+            </div>
+            <div className="formbg-outer">
+              <div className="formbg">
+                <div className="formbg-inner padding-horizontal--48">
+                  <span className="padding-bottom--15">
+                    Sign in to your account
+                  </span>
+                  <FormProvider {...form}>
+                    <form
+                      onSubmit={form.handleSubmit(onSubmit)}
+                    >
+                      <div className="field padding-bottom--24">
+                        <label htmlFor="email">Email</label>
+                        <InputFormItem
+                          name="email"
+                          placeholder={i18n(
+                            'user.fields.email',
+                          )}
+                          autoComplete="email"
+                          autoFocus
+                          externalErrorMessage={
+                            externalErrorMessage
+                          }
+                        />
+                      </div>
+
+                      <div className="field padding-bottom--24">
+                        <div className="grid--50-50">
+                          <label htmlFor="password">
+                            Password
+                          </label>
+                        </div>
+                        <InputFormItem
+                          name="password"
+                          placeholder={i18n(
+                            'user.fields.password',
+                          )}
+                          autoComplete="password"
+                          type="password"
+                        />
+                      </div>
+
+                      <div className="field padding-bottom--24">
+                        <input
+                          type="submit"
+                          name="submit"
+                          defaultValue="Continue"
+                          disabled={loading}
+                        />
+                      </div>
+                      <div className="field">
+                        <ButtonIcon loading={loading} />
+                        {externalErrorMessage && (
+                          <a
+                            className="ssolink"
+                            style={{ color: 'red' }}
+                          >
+                            {externalErrorMessage}
+                          </a>
+                        )}
+                      </div>
+                    </form>
+                  </FormProvider>
+                </div>
+              </div>
+              <div className="footer-link padding-top--24">
+                <span>
+                  <a href="">
+                    <Link
+                      className="btn btn-sm btn-link"
+                      to="/auth/signin"
+                    >
+                      {i18n('auth.alreadyHaveAnAccount')}
+                    </Link>
+                  </a>
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </Wrapper>
   );
 }
